0c8a278c85c9fed880494f276023ee965f2165fa
laundrygrasses <101792540+laundrygrasses@users.noreply.github.com>
2022-04-23 08:15:41 +0800

    Create block_letters.py

---
+++ Python/block_letters.py
@@ -0,0 +1,16 @@
+#CA
+#I'm learning Python
+
+initials =
+
+"""
+  C C C        A
+C       C    A   A
+C          A       A
+C          A A A A A
+C          A       A
+C       C  A       A
+  C C C    A       A
+"""
+
+print(initials)
