47b76c3cd2022566f2a76ce85176ac2e48678e96
laundrygrasses <101792540+laundrygrasses@users.noreply.github.com>
2022-04-23 08:18:52 +0800

    Create len's_slice.py

---
+++ Python/len's_slice.py
@@ -0,0 +1,30 @@
+# Your code below:
+#toppings = ["pepperoni", "pineapple", "cheese", "sausage", "olives", "anchovies", "mushrooms"]
+
+#prices = [2, 6, 1, 3, 2, 7, 2]
+
+#prices.count(2)
+
+#num_pizzas = len(toppings)
+
+#print("We sell " + str(num_pizzas) + " different kinds of pizza!")
+
+pizza_and_prices = [[2, "Pepperoni"], [6, "pineapple"], [1, "cheese"], [3, "sausage"], [2, "olives"], [7, "anchovies"], [2, "mushrooms"]]
+
+#print(pizza_and_prices)
+
+#print(pizza_and_prices)
+
+pizza_and_prices.sort()
+
+cheapest_pizza = pizza_and_prices[0]
+
+priciest_pizza = pizza_and_prices[-1]
+
+pizza_and_prices.pop()
+
+pizza_and_prices.insert(1, [2.5, "peppers"])
+
+three_cheapest = pizza_and_prices[0:3]
+
+print(three_cheapest)
