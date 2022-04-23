753d1f0d9770e45ddfb62a6022fc2efab190d9ad
laundrygrasses <101792540+laundrygrasses@users.noreply.github.com>
2022-04-23 08:17:01 +0800

    Create magic_8-ball.py

---
+++ Python/magic_8-ball.py
@@ -0,0 +1,41 @@
+import random
+
+name = "NAME"
+question = "ASJH"
+answer = ""
+random_number = random.randint(1, 11)
+
+if random_number == 1:
+  answer = "Yes - Definitely."
+elif random_number == 2:
+  answer = "It is decidedly so."
+elif random_number == 3:
+  answer = "Without a doubt."
+elif random_number == 4:
+  answer = "Reply hazy, try again."
+elif random_number == 5:
+  answer = "Ask again later."
+elif random_number == 6:
+  answer = "Better not tell you now."
+elif random_number == 7:
+  answer = "My sources say no."
+elif random_number == 8:
+  answer = "Outlook not so good."
+elif random_number == 9:
+  answer = "Very doubtful."
+elif random_number == 10:
+  answer = "Why are you asking a ball for advice?"
+elif random_number == 11:
+  answer = "Yeah, no."
+else:
+  answer = "Error"
+
+if name == "":
+  print("Question: " + question)
+else:
+  print(name + " asks: " + question)
+
+if question == "":
+  print("The Magic 8-Ball cannot provide a fortune unless you ask it something.")
+
+print("Magic 8-Ball's answer: " + answer)
